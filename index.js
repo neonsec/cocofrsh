26128c3d3f97eea595d028c2fad3a15201e76618
fahidroid <fahidroid@gmail.com>
2017-04-16 07:07:22 +0530

    new site

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
-
+var port = process.env.PORT || 7000
 var app = express();
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
 res.send(__dirname+'/public/index.html');
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(port,()=>{
   console.log('server started')
 })
